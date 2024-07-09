910e077d6f1b83aa4961f7125a8e1bc48a155125
DimasCW <cdimas292@gmail.com>
2024-07-10 00:21:25 +0700

    huu

--- components/login/index.js
+++ components/login/index.js
@@ -47,7 +47,7 @@ const Login = () => {
         <div>
           <TextInput id="password1" placeholder="Password ..." required type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <Button className="mb-4" color="failure" type="submit">Login</Button>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
       </form>
       <p>Do you have account? <Link href="/registerpage" className="font-bold">Register Now</Link></p>
     </Layout>
